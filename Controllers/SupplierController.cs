193bfaa0ea39a4ab535298aa5a8036e2534b8d48
1hphuc <phucb1909971@student.ctu.edu.vn>
2022-09-25 17:16:42 +0700

    Add SupplierController Edit Site.css Edit Home index Edit Staff index

---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,40 @@
+﻿using PagedList;
+using QLNS.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNS.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly csdlEntities db = new csdlEntities();
+
+        public ActionResult IsLogin()
+        {
+            ActionResult res = null;
+            if (!SessionHelper.IsLogin())
+            {
+                TempData["error"] = "Vui lòng đăng nhập vào hệ thống";
+                res = RedirectToAction("Index", "Auth");
+            }
+            return res;
+        }
+        public IEnumerable<NHA_CUNG_CAP> BooksPaged(int? page)
+        {
+            int PageSize = 4;
+            int PageNumber = (page ?? 1);
+            return db.NHA_CUNG_CAP.OrderBy(x => x.MA_NCC).ToPagedList(PageNumber, PageSize);
+        }
+        // GET: Staff
+        public ActionResult Index(int? page)
+        {
+            if (IsLogin() != null) return IsLogin();
+            if (page == null) page = 1;
+            var list = BooksPaged(page);
+            return View(list);
+        }
+    }
+}
